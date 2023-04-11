1bc1e82f29b9f6c315611bace9f7e3850b65dd16
ilyas <muhammadilyazafzal@gmail.com>
2023-04-11 19:25:54 +0500

    half

---
+++ app.js
@@ -0,0 +1,46 @@
+// Question 01: Closure
+/*
+function addNum (addNumber){
+    return function(val){
+        console.log(val + addNumber);
+    }
+}
+ let result = addNum(5);
+ result(8);
+ */
+// Question 02:  Recursive Function
+/*
+ let a = [1,2,3,4,5,6,7];
+ 
+ function searching(arr,val){
+    if (arr.length === 0){
+       return false;
+    }else if(arr[0]=== val){
+    return true;
+    }else {
+        arr.shift();
+        return searching(arr,val);
+    }
+ };
+console.log(searching(a, 8)); 
+*/
+// Question 03:   Add  New Paragraph Element
+/*
+let Paragraph = [];
+let  para = document.getElementById("my-para");
+function paraAdd(arg){
+    let data1 = prompt("Enther data");
+    Paragraph.push(data1);
+ para.innerHTML = "<p><b> " + arg +"</b></p>"
+ // Paragraph.shift();
+}
+*/
+// Question 04:
+/*
+let list =document.getElementById("my-list");
+function additem(){
+    list.innerHTML = "<ol> <li>List item 4</li> </ol>"
+}
+*/
+// / Question 05:
+
